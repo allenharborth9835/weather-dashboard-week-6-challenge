8ce4a9b825b74443c5048f687ac5632ae17d7e4d
Harborth, Allen <allenharborth9835@gmail.com>
2021-07-31 10:57:33 -0500

    add 5 day weather and functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,5 @@
-//create a var to store the current city
-var city;
 //var to store the past city that have been searched
-var cityArr = [];
-//var to see if city exist
+var citysArr = [];
 
 
 function searchButtonHandler(){
@@ -17,12 +14,12 @@ function cityButtonHandler(){
 }
 
 function buildButton(city){
-    if(!(cityArr.includes(city))){
+    if(!(citysArr.includes(city))){
         let cityButtonEl = document.createElement("button");
         $(cityButtonEl).text(city);
-        $(cityButtonEl).addClass("cityBtn");
+        $(cityButtonEl).addClass("col-12 cityBtn");
         $("#citys").append(cityButtonEl);
-        cityArr+=city;
+        citysArr+=city;
         return;
     }
     else{
@@ -31,15 +28,11 @@ function buildButton(city){
 }
 
 function search(city){
-    let currentDate = new Date();
-    let cDay = currentDate.getDate();
-    let cMonth = currentDate.getMonth() + 1;
-    let cYear = currentDate.getFullYear();
-    let date = `${cMonth}/${cDay}/${cYear}`;
+    $("#daily").html("")
+    let date = `${new Date().getMonth()+1}/${new Date().getDate()}/${new Date().getFullYear()}`;
     apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=df58c4381be6ff126a86743012407847&units=imperial`;
     fetch(apiUrl).then(response => response.json())
         .then(function(data){
-            console.log(data)
             if(data.cod > 300){
                 alert(data.message);
                 return false;
@@ -49,17 +42,41 @@ function search(city){
                 let wind = data.wind.speed;
                 let humidity = data.main.humidity;
                 let lat = data.coord.lat;
-                let lon = data.coord.lat;
-                let info = `<h1>${name} ${date}</h1><p>temp:${temp}°F<p><p>wind:${wind} MPH</p><p>humidity:${humidity}%</p>`;
+                let lon = data.coord.lon;
+                let source = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+                let info = `<h1>${name} (${date})<img src=${source}></h1>
+                    <p>temp:${temp}°F</p>
+                    <p>wind:${wind} MPH</p>
+                    <p>humidity:${humidity}%</p>`;
                 $("#weather").html(info);
                 buildButton(name)
-                apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=df58c4381be6ff126a86743012407847`;
+                apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=df58c4381be6ff126a86743012407847&units=imperial`;
                 return fetch(apiUrl);
             }
         }).then(response=> response.json())
         .then(function(data){
+            console.log(data)
             let uv= data.daily[0].uvi;
             $("#weather").append(`<p>uv:${uv}</p>`);
+            for(let i = 0; i<5; i++){
+                let date = `${new Date().getMonth()+1}/${new Date().getDate()+1+i}/${new Date().getFullYear()}`;
+                let source = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`;
+                console.log(source)
+                let temp = data.daily[i].temp.day;
+                let wind = data.daily[i].wind_speed;
+                let Humidity = data.daily[i].humidity;
+                let info = `<div class='col-12 col-md-4 col-xl-2 mb-3'>
+                    <div class='card forcastCard'>
+                    <h3>${date}</h3>
+                    <img width="50" height="50" src=${source}>
+                    <p>Temp:${temp}°F</p>
+                    <p>Wind:${wind} MPH</p>
+                    <p>Humidity:${Humidity}%</p>
+                    </div></div>`
+                $("#daily").append(info)
+
+            }
+            return false;
         })
 }
 
